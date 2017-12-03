4e37961ee516d6d0f2796cb2f0bb3ab13a7fad63
jd <duvaljd@umich.edu>
2017-12-03 13:49:17 -0500

    done

--- sample_flask_app.py
+++ sample_flask_app.py
@@ -1,7 +1,8 @@
 # Import statements necessary
 from flask import Flask, render_template
 from flask_script import Manager
-
+import requests
+import json
 # Set up application
 app = Flask(__name__)
 
@@ -30,14 +31,24 @@ def basic_values_list(name):
 
 
 ## PART 1: Add another route /word/<new_word> as the instructions describe.
+@app.route('/word/<new_word>')
+def rhyme_word(new_word):
+    baseURL = "https://api.datamuse.com/words"
+    params_dxn = {}
+    params_dxn["rel_rhy"] = new_word
 
+    resp = requests.get(baseURL, params = params_dxn)
+    resp_str = resp.text
+    muse = json.loads(resp_str)
+
+    return muse[0]["word"]
 
 ## PART 2: Edit the following route so that the photo_tags.html template will render
 @app.route('/flickrphotos/<tag>/<num>')
 def photo_titles(tag, num):
     # HINT: Trying out the flickr accessing code in another file and seeing what data you get will help debug what you need to add and send to the template!
     # HINT 2: This is almost all the same kind of nested data investigation you've done before!
-    FLICKR_KEY = "" # TODO: fill in a flickr key
+    FLICKR_KEY = "f8ad7e14472778a7561bc802415962cd" # TODO: fill in a flickr key
     baseurl = 'https://api.flickr.com/services/rest/'
     params = {}
     params['api_key'] = FLICKR_KEY
@@ -49,9 +60,15 @@ def photo_titles(tag, num):
     response_obj = requests.get(baseurl, params=params)
     trimmed_text = response_obj.text[14:-1]
     flickr_data = json.loads(trimmed_text)
+
     # TODO: Add some code here that processes flickr_data in some way to get what you nested
+    results = len(flickr_data["photos"]["photo"])
+    titles = []
+    for dxn in flickr_data["photos"]["photo"]:
+        titles.append(dxn["title"])
+    
     # TODO: Edit the invocation to render_template to send the data you need
-    return render_template('photo_tags.html')
+    return render_template('photo_info.html', num = results, photo_titles = titles)
 
 
 
